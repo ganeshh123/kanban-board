824900389ea6bc6a4a2c982a85349fae7b2d0dbc
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2021-01-05 11:13:39 +0000

    69879

--- src/Task.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-class Task extends React.Component {
-    render = () => {
-      return <h1>Hello</h1>;
-    }
-  }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,23 @@
-let task = <Task />
-ReactDOM.render(task, document.body.appendChild(document.createElement("DIV")))
+let React = window.React
+let ReactDOM = window.ReactDOM
+let {DragDropContext, Draggable, Droppable } = window.ReactBeautifulDnd;
+
+class Task extends React.Component {
+
+
+    render = () => {
+        let task = this.props.task
+        
+        return(
+            <div class="taskCard">
+                <p class="taskCardContent">
+                    Buy Milk at the store
+                </p>
+                <img class="icon" src="./assets/cancel-circle.svg" alt="Delete Task"></img>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<Task />, document.querySelector('body'))
