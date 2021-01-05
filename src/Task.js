07bd674dcdc934326f4f1849ab73cd3bb557d745
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2021-01-05 10:01:14 +0000

    reactbabel

---
+++ src/Task.js
@@ -0,0 +1,7 @@
+'use strict'
+
+class Task extends React.Component {
+    render = () => {
+      return <h1>Hello</h1>;
+    }
+  }
---
+++ src/main.js
@@ -0,0 +1,2 @@
+let task = <Task />
+ReactDOM.render(task, document.body.appendChild(document.createElement("DIV")))
