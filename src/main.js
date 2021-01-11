216a1c29b0bbc9a80b7847c5ba361529da8a0bcd
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2021-01-11 13:54:34 +0000

    sigprog

--- src/main.js
+++ src/main.js
@@ -2,24 +2,69 @@ let React = window.React
 let ReactDOM = window.ReactDOM
 let {DragDropContext, Draggable, Droppable } = window.ReactBeautifulDnd;
 
-//console.log(Math.random().toString(36).substring(2, 15))
+let lists = {}
 
-let list1 = [
-    {"id" : "v0zigt47whc", "taskContent" : "Buy Milk"},
-    {"id" : "b0zi5t4rwhc", "taskContent" : "Work Out"}
-]
+let storeList = () => {
+    console.log('Writing Updated Lists to Storage')
+    console.log(lists)
+    console.log('List Storage Updated ')
+    window.localStorage.setItem('listStore', JSON.stringify(lists))
+    console.log('Set Correctly')
+    lists = JSON.parse(localStorage.getItem('listStore'))
+}
 
+let editableItems = () => {
+    /* Editable List Name */
+    let allListNameDOM = document.querySelectorAll('.listName')
+    allListNameDOM.forEach((listNameDOM) => {
+        listNameDOM.contentEditable = true
+        listNameDOM.addEventListener('keyup', (event) => {
+            if(event.which == 13 && document.activeElement == listNameDOM){
+                let listId = event.target.parentNode.id
+                lists[listId]['listName'] = event.target.innerText
+                event.target.childNodes[1].remove()
+                storeList()
+                listNameDOM.blur()
+            }
+        })
+    })
+
+    /* Editable List Items */
+    let allTaskCardContentDOM = document.querySelectorAll('.taskCardContent')
+    allTaskCardContentDOM.forEach((taskCardContentDOM) => {
+        taskCardContentDOM.contentEditable = true
+        taskCardContentDOM.addEventListener('keyup', (event) => {
+            if(event.which == 13 && document.activeElement == taskCardContentDOM){
+                let taskId = event.target.parentNode.id
+                let listId = event.target.parentNode.parentNode.parentNode.id
+                lists[listId]['listItems'][taskId]['taskContent'] = event.target.innerText
+                event.target.childNodes[1].remove()
+                storeList()
+                taskCardContentDOM.blur()
+            }
+        })
+    })
+
+}
 
 class Task extends React.Component {
+
+    deleteTaskClicked = (event) => {
+        let taskId = event.target.parentNode.id
+        let listId = event.target.parentNode.parentNode.parentNode.id
+        lists[listId]['listItems'][taskId] = undefined
+        storeList()
+        setTimeout(this.props.refresh(), 1000)
+    }
 
     render = () => {
 
         return(
-            <div class="taskCard">
+            <div class="taskCard" id={this.props.task['id']}>
                 <p class="taskCardContent">
                    {this.props.task['taskContent']}
                 </p>
-                <img class="icon" src="./assets/cancel-circle.svg" alt="Delete Task"></img>
+                <img class="icon deleteTaskIcon" src="./assets/cancel-circle.svg" alt="Delete Task" onClick={this.deleteTaskClicked}/>
             </div>
         )
     }
@@ -27,21 +72,97 @@ class Task extends React.Component {
 
 class List extends React.Component {
 
+    addTaskClicked = (event) => {
+        let listId = event.target.parentNode.parentNode.id
+        let newTaskId = Math.random().toString(36).substring(2, 15)
+        let newTaskContent = document.querySelector('#' + listId).childNodes[2].childNodes[0].value
+        if(newTaskContent && newTaskContent != ''){
+            lists[listId]['listItems'][newTaskId] = {
+                'id': newTaskId,
+                'taskContent': newTaskContent
+            }
+        }
+        storeList()
+        document.querySelector('#' + listId).childNodes[2].childNodes[0].value = ''
+        setTimeout(this.props.refresh(), 2000)
+    }
+
     render = () => {
         return(
-            <div id="listContainer">
+            <div class="listContainer" id={this.props.list['id']}>
+                <h4 class="listName">{this.props.list['listName']}</h4>
                 <div id="taskList">
-                    {this.props.list.map((listItem, index) => {
-                        return <Task id={listItem['id']} task={listItem} />
+                    {Object.values(this.props.list['listItems']).map((listItem) =>{
+                        if(listItem){
+                            return <Task 
+                                    id={listItem['id']} 
+                                    task={listItem} 
+                                    refresh = {this.props.refresh}
+                                />
+                        }
                     })}
                 </div>
-                <div id="newListItem">
-
+                <div class="newListItem">
+                    <input type="text" class="newListItemInput" placeholder="New Task ..." />
+                    <img 
+                        class="icon addTaskIcon"
+                        src="./assets/plus.svg"
+                        alt="Add Task"
+                        onClick={this.addTaskClicked}
+                    />
                 </div>
             </div>
         )
     }
 }
 
+class App extends React.Component{
 
-ReactDOM.render(<List list={list1} />, document.querySelector('body'))
+    state = {
+        appLists: lists
+    }
+
+    refreshApp = () => {
+        console.log('Refreshing...')
+        this.setState({
+            appLists: lists
+        })
+        editableItems()
+    }
+
+    render = () => {
+            return(
+                <div id="app">
+                    {Object.values(this.state.appLists).map((list) => {
+                        return <List list={list} refresh={this.refreshApp}/>
+                    })}
+                </div>
+            )
+    }
+}
+
+let startUp = () => {
+
+    if(localStorage.getItem('listStore') && !['undefined', 'null'].includes(localStorage.getItem('listStore'))){
+        lists = JSON.parse(localStorage.getItem('listStore'))
+    }else{
+        console.log('Creating List in Storage')
+        lists = {}
+        let list1 = {
+            "id" : "y0zitt47gfb",
+            "listName": "To Do",
+            "listItems" : {
+                "v0zigt47whc": {"id" : "v0zigt47whc", "taskContent" : "Buy Milk"},
+                "b0zi5t4rwhc" : {"id" : "b0zi5t4rwhc", "taskContent" : "Work Out"}
+            }
+        }
+        lists['y0zitt47gfb'] = list1
+        storeList()
+    }
+
+    ReactDOM.render(<App />, document.querySelector('body'))
+}
+
+startUp()
+
+
